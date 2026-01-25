b121ebc7cc59b3321c64ca28705a7404fca0fb4f
Colin <jay_jieliu@outlook.com>
2026-01-25 23:28:25 +0800

    up

--- MIT6.824-2021/README.md
+++
@@ -1,2 +0,0 @@
-## Reminding
-Please try your best to finish this lab by yourself~
--- MIT6.824-2021/src/all-test.sh
+++
@@ -1,7 +0,0 @@
-go test raft/*.go -v
-
-go test kvraft/*.go -v
-
-go test shardctler/*.go -v
-
-go test shardkv/*.go -v
